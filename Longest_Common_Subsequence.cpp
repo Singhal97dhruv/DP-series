499b5312789ddd11acf13fbfa965629c39cdc959
Dhruv <Dhruvsinghal9876@gmail.com>
2022-05-31 00:14:21 +0530

    adding dp approaches to solve problem

--- Longest_Common_Subsequence.cpp
+++ Longest_Common_Subsequence.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 #include<string>
 
 using namespace std;
@@ -13,7 +14,32 @@ int lcs(string X,string Y,int m,int n){
     }
 }
 
+vector<vector<int>>mem(10001,vector<int>(10001,-1));
+int memorization(string X,string Y,int m,int n){
+    if(m==0 || n==0)
+    {
+        return 0;
+    }
+    if(mem[m][n]!=-1)return mem[m][n];
+    if(X[m-1]==Y[n-1])return mem[m][n]=1+memorization(X,Y,m-1,n-1);
+    else return mem[m][n]=max(memorization(X,Y,m-1,n),memorization(X,Y,m,n-1));
+}
 
+vector<vector<int>>dp(10001,vector<int>(10001));
+void dprog(string X,string Y,int m,int n){
+    // for(int i=0;i<=m;i++){
+    //     for(int j=0;j<=n;j++){
+    //         if(i==0 || j==0)
+    //         dp[i][j]=0;
+    //     }
+    // }
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(X[i-1]==Y[j-1])dp[i][j]=1+dp[i-1][j-1];
+            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+}
 
 int main(){
     string X,Y;
@@ -24,9 +50,11 @@ int main(){
     cout<<"For string y: ";
     cin>>Y;
     //For finding the length of the longest common subsequence
-    cout<<lcs(X,Y,X.size(),Y.size());       // Recursive approach
+    cout<<"Through recursive approach ->"<<lcs(X,Y,X.size(),Y.size())<<endl;       // Recursive approach
 
+    mem.resize(X.size(),vector<int>(Y.size(),-1));
+    cout<<"Through bottom up approach ->"<<memorization(X,Y,X.size(),Y.size())<<endl;
 
-
-
+    dprog(X,Y,X.size(),Y.size());
+    cout<<"Through top down approach ->"<<dp[X.size()][Y.size()]<<endl;
 }
